77405809e0a5a732ddab8099aae66c65fa08c14e
SaharSajjad31 <116629355+SaharSajjad31@users.noreply.github.com>
2024-07-23 17:53:37 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -124,8 +124,8 @@ LOGIN_REDIRECT_URL = '/'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'maryamrehman1154@gmail.com'
-EMAIL_HOST_PASSWORD = 'maryammohsin1154'
+EMAIL_HOST_USER = 'sahsaj31@gmail.com'
+EMAIL_HOST_PASSWORD = '31star01'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
